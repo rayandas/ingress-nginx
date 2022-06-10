f51f103c298dcd9e7fcb3064c2c375f6e2b9a461
Sameer Kulkarni <samkulkarni20@gmail.com>
2022-06-10 15:26:17 +0530

    fix drone build failure
    `go install ginkgo` followed by `which ginkgo` is newly added in the upstream repo.
    This is making the drone build fail for arm64 arch. The ginkgo library
    gets installed under $GOPATH/bin/linux_arm64 dir. This is unlike amd64 images
    which typically install go libraries under $GOPATH/bin. Since the previously mentioned
    dir is not in PATH, the command `which ginkgo` fails. I've added this location to PATH
    to fix the build failure. See upstream PRs linked below for more info:
    https://github.com/kubernetes/ingress-nginx/pull/8566
    https://github.com/kubernetes/ingress-nginx/pull/8569

--- build/run-in-docker.sh
+++ build/run-in-docker.sh
@@ -79,6 +79,7 @@ if [[ "$DOCKER_IN_DOCKER_ENABLED" == "true" ]]; then
   set -x
   go install -mod=mod github.com/onsi/ginkgo/v2/ginkgo@v2.1.4
   find / -type f -name ginkgo 2>/dev/null
+  PATH=$PATH:$GOPATH/bin/linux_arm64
   which ginkgo
   /bin/bash -c "${FLAGS}"
   set +x
